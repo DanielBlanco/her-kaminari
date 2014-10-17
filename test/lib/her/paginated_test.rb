2292b957f09a35ce450608110b107de7e8edf50f
Daniel Blanco Rojas <daniel.blancorojas@gmail.com>
2014-10-17 17:45:23 -0600

    Refactoring tests

---
+++ test/lib/her/paginated_test.rb
@@ -0,0 +1,12 @@
+# -*- encoding : utf-8 -*-
+# test/lib/her/paginated_test.rb
+require_relative '../../test_helper'
+
+describe PaginatedTest do
+
+  it "must be defined" do
+    Her::Paginated::VERSION.wont_be_nil
+  end
+
+end
+
